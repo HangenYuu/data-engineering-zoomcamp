eb49161869eaf9fb13ec9e87a221d7958341f99a
HangenYuu <97870091+HangenYuu@users.noreply.github.com>
2024-03-09 21:53:10 +0800

    Finished Lesson 1. Moved to homework.

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_basic/main.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_basic/main.tf
@@ -1,18 +1,18 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.19.0"
     }
   }
 }
 
 provider "google" {
-  project     = "dezoomcamp-416702"
-  region      = "us-central1"
+  project = "dezoomcamp-416702"
+  region  = "us-central1"
 }
 
-resource "google_storage_bucket" "auto-expire" {
+resource "google_storage_bucket" "demo_bucket" {
   name          = "dezoomcamp-416702-terra-bucket"
   location      = "US"
   force_destroy = true
@@ -24,4 +24,8 @@ resource "google_storage_bucket" "auto-expire" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = "terra_dataset"
 }
